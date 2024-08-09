c71afcf24a971ab143b35c8f0e76d0577d4cbdf1
shivanshumishra <shivanshufeb14@gmail.com>
2024-08-10 00:48:20 +0530

    added dependencies

--- app/src/main/java/com/shivanshu/diary/MainActivity.kt
+++ app/src/main/java/com/shivanshu/diary/MainActivity.kt
@@ -21,25 +21,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Text(text = "Diary")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiaryTheme {
-        Greeting("Android")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("io.realm.kotlin") version "1.11.0" apply false
+    id ("com.google.dagger.hilt.android") version "2.48" apply false
+//    id ("com.google.gms.google-services") version "4.3.14" apply false
+    id ("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
 }
